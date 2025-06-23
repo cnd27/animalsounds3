93c80f43494da38d979864bc38b73df0c7ce8e24
Christopher Davis <C.Davis.7@warwick.ac.uk>
2025-06-23 14:26:09 +0100

    updated documentation

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,3 +1,12 @@
+#' Print the sounds animals make
+#'
+#' @param animal A string - the name of the animal
+#' @param sound A string - the name of the sound
+#'
+#' @returns A string - 'The \{animal\} says \{sound\}!'
+#' @export
+#'
+#' @examples animal_sounds('dog','woof')
 animal_sounds <- function(animal, sound) {
   stopifnot(is.character(animal) & length(animal) == 1)
   stopifnot(is.character(sound) & length(sound) == 1)
