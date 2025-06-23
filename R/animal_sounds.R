6f54929ef080758e910c656779bdcf3e1ae08e69
Christopher Davis <C.Davis.7@warwick.ac.uk>
2025-06-23 15:19:57 +0100

    adding test functions

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -17,13 +17,16 @@ animal_sounds <- function(animal, sound) {
       c("{.var animal} must be a single string!",
         "i" = "It was {.type {animal}} of length {length(animal)} instead.",
         "i" = "You typed {animal}."),
+      class = "error_not_single_string"
     )
   }
 
   if (!rlang::is_character(sound, n = 1)) {
     cli::cli_abort(
       c("{.var sound} must be a single string!",
-        "i" = "It was {.type {sound}} of length {length(sound)} instead.")
+        "i" = "It was {.type {sound}} of length {length(sound)} instead.",
+        "i" = "You typed {sound}."),
+      class = "error_not_single_string"
     )
   }
   paste0("The ", animal, " says ", sound, "!")
